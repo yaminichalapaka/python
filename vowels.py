f4ea37728767926ea315d59ac25d9a04bdf20834
yaminichalapaka <103511540+yaminichalapaka@users.noreply.github.com>
2023-06-21 19:01:36 +0530

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,8 @@
+print("Enter a character")
+c = input() 
+if (c== 'a' or c == 'e' or c == 'i' or c == 'o' or c == 'u' or c== 'A' or c == 'E' or c == 'I' or c == 'O' or c == 'U'): 
+    print("Vowel") 
+elif ((c >= 'A' and c <= 'Z') or (c >= 'a' and c <= 'z')):
+    print("Consonant")
+else:
+    print("Invalid input")
